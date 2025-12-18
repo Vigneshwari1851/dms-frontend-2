bc51743c9cbea11bd7cabe22fc7f80b673ccf5b4
Plavanya18 <prakashlavanya18@gmail.com>
2025-12-18 10:21:18 +0530

    toast

--- src/components/common/Toast.jsx
+++ src/components/common/Toast.jsx
@@ -4,19 +4,6 @@ import failIcon from "../../assets/toast/fail.svg";
 import pendingIcon from "../../assets/toast/pending.svg";
 
 export default function Toast({ show, message, type }) {
-    const getGradient = () => {
-        switch (type) {
-            case "success":
-                return "bg-[radial-gradient(circle_at_top_left,_#00ED51,_#00ED7B00),_#2E3439]";
-            case "error":
-                return "bg-[radial-gradient(circle_at_top_left,_#EB1D2E,_#EB1D2E00),_#2E3439]";
-            case "pending":
-                return "bg-[radial-gradient(circle_at_top_left,_#FFCC00,_#D8AD0000),_#2E3439]";
-            default:
-                return "bg-[#2E3439]";
-        }
-    };
-
     const getIcon = () => {
         if (type === "success") return successIcon;
         if (type === "error") return failIcon;
@@ -32,22 +19,18 @@ export default function Toast({ show, message, type }) {
                         flex items-center gap-4
                         w-[459px] h-14
                         px-4 py-3
-                        
                         rounded-lg
                         text-white text-[16px] font-medium
-                        
+                        bg-[#2E3439]
                         shadow-[0px_8px_10px_0px_#00000033,
                                 0px_6px_30px_0px_#0000001F,
                                 0px_16px_24px_0px_#00000024]
-                        
                         backdrop-blur-xl
                         transition-all duration-300
-                        
-                        ${getGradient()}
                         ${show ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-2"}
-                    `}
+          `}
                 >
-                    <img src={getIcon()} alt="icon" className="w-6 h-6" />
+                    <img src={getIcon()} alt="icon" className="w-20 h-20" />
                     <span className="whitespace-nowrap leading-none">{message}</span>
                 </div>
             )}
