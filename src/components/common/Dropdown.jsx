ba1476fd2df3b85ae4b6be8ed178f9a659ecf43b
Plavanya18 <prakashlavanya18@gmail.com>
2025-12-19 11:00:33 +0530

    dropdown scroll added

--- src/components/common/Dropdown.jsx
+++ src/components/common/Dropdown.jsx
@@ -41,7 +41,7 @@ export default function Dropdown({
             </button>
 
             {open && (
-                <ul className="absolute w-full mt-2 bg-[#2E3439] border border-[#2A2F33] rounded-lg z-10">
+                <ul className="w-full mt-2 bg-[#2E3439] border border-[#2A2F33] rounded-lg z-10  max-h-48 overflow-y-auto scrollbar-dark relative">
                     {options.map((item, index) => {
                         const value = typeof item === "string" ? item : item.label;
 
