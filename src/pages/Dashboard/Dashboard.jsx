a7be9461ee70eeaa9e4efce88d49ae9e556ca7ed
Plavanya18 <prakashlavanya18@gmail.com>
2026-03-02 11:56:51 +0530

    remove

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -83,7 +83,7 @@ export default function Dashboard() {
       </div>
 
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-6 mb-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-6 mb-4">
         <StatCard
           title="Today's Deals"
           value={stats.today?.count || 0}
